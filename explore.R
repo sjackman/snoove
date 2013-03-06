a2977f32aa973842531f6b41784f834f8787f657
Shaun Jackman <sjackman@gmail.com>
2013-03-05 17:21:43 -0800

    Explore a VCF file

---
+++ explore.R
@@ -0,0 +1,40 @@
+# Explore a VCF file
+
+#source('http://bioconductor.org/biocLite.R')
+#biocLite('VariantAnnotation')
+
+library('lattice')
+library('reshape2')
+library('VariantAnnotation')
+
+vcf <- readVcf('98E.vcf.gz', 'NC_018521')
+
+# Histogram of the quality values.
+qual <- fixed(vcf)$QUAL
+histogram(qual, main='Histogram of quality')
+
+# Violin plot of the quality vs. genotype.
+gt <- data.frame(geno(vcf)$GT)
+qual.gt <- melt(cbind(qual, gt), id.vars='qual',
+	variable.name='sample', value.name='gt')
+qual.gt$gt <- factor(qual.gt$gt)
+bwplot(qual ~ gt, qual.gt, panel = panel.violin,
+	main='Quality vs. genotype',
+	ylab='Quality', xlab='Genotype')
+bwplot(qual ~ gt | sample, qual.gt, panel = panel.violin,
+	main='Quality vs. genotype',
+	ylab='Quality', xlab='Genotype')
+
+# Examine genotype qualities.
+gt.gq <- data.frame(gt = geno(vcf)$GT, gq=geno(vcf)$GQ)
+gt <- melt(t(geno(vcf)$GT), value.name='gt')
+gq <- melt(t(geno(vcf)$GQ), value.name='gq')
+gt.gq <- cbind(gt[1], gt$gt, gq$gq)
+colnames(gt.gq) <- c('sample', 'gt', 'gq')
+histogram(gt.gq$gq, main='Histogram of genotype quality')
+bwplot(gq ~ gt, gt.gq, panel = panel.violin,
+	main='Genotype quality vs. genotype',
+	ylab='Genotype quality', xlab='Genotype')
+bwplot(gq ~ gt | sample, gt.gq, panel = panel.violin,
+	main='Genotype quality vs. genotype',
+	ylab='Genotype quality', xlab='Genotype')
